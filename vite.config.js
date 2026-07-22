be3c24a656dfb7996eba6e98b03371b8e38f553a
Darshan Nimodiya <darshanagrawal16@gmail.com>
2026-07-22 18:49:18 +0530

    Fix Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Skill.AI/",
+  base: process.env.GITHUB_ACTIONS ? "/Skill.AI/" : "/",
 });
